87014b969f278602ac56eb4dd89ed3b4910c3edb
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2023-12-29 06:28:17 -0300

    add Habilidades section + link event on menu

--- static/script.js
+++ static/script.js
@@ -5,3 +5,19 @@ menuMobile.addEventListener("click", () => {
     menuMobile.classList.contains("bi-list") ? menuMobile.classList.replace("bi-list", "bi-x") : menuMobile.classList.replace("bi-x", "bi-list")
     body.classList.toggle("menu-nav-active")
 })
+
+
+// Close menu when click on link
+
+const navItem = document.querySelectorAll(".nav-item")
+
+
+navItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
+})
+console.log(navItem)
