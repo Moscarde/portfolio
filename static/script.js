504f5d30143166d5e2aac6cb51deb5088f368ce5
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2024-01-07 23:59:13 -0300

    add animations

--- static/script.js
+++ static/script.js
@@ -21,3 +21,27 @@ navItem.forEach((item) => {
     })
 })
 console.log(navItem)
+
+
+// Animations
+
+const item = document.querySelectorAll("[data-anime]")
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.90;
+
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate")
+        } else {
+            element.classList.remove("animate")
+        }
+    })
+}
+
+animeScroll()
+
+window.addEventListener("scroll", () => {
+    
+    animeScroll()
+})
